667f43ebbb9d00279d2f1fa9f707b0fac99a8caa
ovomallouk <ismaiil.mallouk@live.fr>
2017-06-01 17:48:16 +0200

    Index page

--- KiabiHackatonAmaris/Controllers/HomeController.cs
+++ KiabiHackatonAmaris/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using KiabiHackatonAmaris.EnumClass;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+
 
 namespace KiabiHackatonAmaris.Controllers
 {
@@ -25,6 +27,18 @@ namespace KiabiHackatonAmaris.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public JsonResult GetStores()
+        {
+
+
+            var data = Enum.GetValues(typeof(StoreEnum)).Cast<StoreEnum>().Select(e => new
+            {
+                id = (int)e,
+                text = e.ToString()
+            });
+            return Json(data, JsonRequestBehavior.AllowGet);
         }
     }
 }
