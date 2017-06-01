47d7f1e635097b7389ded24d49709b45197456ff
Daniel Marques <daniel.marques@amaris.com>
2017-06-01 12:02:51 +0200

    Add project files.

---
+++ KiabiHackatonAmaris/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KiabiHackatonAmaris.Startup))]
+namespace KiabiHackatonAmaris
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
